c7584f5644142e769802a4ac77c1e1027119e99b
Vladislav Rets <retz.vv@gmail.com>
2023-02-20 20:27:02 +0700

    docs(Model): added XML comments

--- Lab1/Model/Gender.cs
+++ Lab1/Model/Gender.cs
@@ -1,14 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Model
 {
+    /// <summary>
+    /// Variants of person's gender.
+    /// </summary>
     public enum Gender
     {
+        /// <summary>
+        /// Male gender.
+        /// </summary>
         Male,
+
+        /// <summary>
+        /// Female gender.
+        /// </summary>
         Female
     }
 }
--- Lab1/Model/Person.cs
+++ Lab1/Model/Person.cs
@@ -1,15 +1,101 @@
 namespace Model
 {
+    /// <summary>
+    /// Class which describe persons.
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Name of person.
+        /// </summary>
         private string _name;
 
+        /// <summary>
+        /// Surname of person.
+        /// </summary>
         private string _surname;
 
+        /// <summary>
+        /// Age of person.
+        /// </summary>
         private int _age;
 
+        /// <summary>
+        /// Gender of person.
+        /// </summary>
         private Gender _gender;
 
+        /// <summary>
+        /// Enter the name of person.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Enter the surname of person.
+        /// </summary>
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+
+            private set
+            {
+                _surname = value;
+            }
+        }
+
+        /// <summary>
+        /// Enter the age of person.
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            private set
+            {
+                _age = value;
+            }
+        }
+
+        /// <summary>
+        /// Enter the gender of person.
+        /// </summary>
+        public Gender Gender
+        {
+            get
+            {
+                return _gender;
+            }
+
+            private set
+            {
+                _gender = value;
+            }
+        }
+
+        /// <summary>
+        /// Person's constructor.
+        /// </summary>
+        /// <param name="name">Name of person.</param>
+        /// <param name="surname">Surname of person.</param>
+        /// <param name="age">Age of person.</param>
+        /// <param name="gender">Gender of person.</param>
         public Person(string name, string surname, int age, Gender gender)
         {
             _name = name;
