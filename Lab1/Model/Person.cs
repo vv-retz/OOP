8125cc8061859edbb7e35b98651940289be37b15
Vladislav Rets <retz.vv@gmail.com>
2023-02-20 18:39:48 +0700

    features(Model): added Gender, modified Person
    -class Gender (enum) added  in Model
    -fields and constructor added in Person

--- Lab1/Model/Person.cs
+++ Lab1/Model/Person.cs
@@ -2,6 +2,21 @@ namespace Model
 {
     public class Person
     {
+        private string _name;
 
+        private string _surname;
+
+        private int _age;
+
+        private Gender _gender;
+
+        public Person(string name, string surname, int age, Gender gender)
+        {
+            _name = name;
+            _surname = surname;
+            _age = age;
+            _gender = gender;
+
+        }
     }
 }
