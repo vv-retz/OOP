37336360e0477530cc7a8fc337526ab31ee288c7
Vladislav Rets <retz.vv@gmail.com>
2023-02-22 09:50:15 +0700

    feature(Model): added the ability to add persons at the list

--- Lab1/Model/PersonList.cs
+++ Lab1/Model/PersonList.cs
@@ -1,10 +1,24 @@
 namespace Model
 {
     /// <summary>
-    /// Class which describe List of persons.
+    /// Class which describe list of persons (as an array).
     /// </summary>
     internal class PersonList
     {
+        /// <summary>
+        /// Array of persons.
+        /// </summary>
+        private Person[] _arrayOfPersons = new Person[0];
 
+        /// <summary>
+        /// Function for add a person at the end of the array.
+        /// </summary>
+        /// <param name="person">The person being added.</param>
+        public void AddPerson(Person person)
+        {
+            var indexOfNewPerson = _arrayOfPersons.Lenght;
+            Array.Resize(ref _arrayOfPersons, indexOfNewPerson + 1);
+            _arrayOfPersons[indexOfNewPerson] = person;
+        }
     }
 }
