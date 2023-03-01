a4d1fe4ce62e5206696121b7269ec2bf5cb021a2
Vladislav Rets <retz.vv@gmail.com>
2023-03-01 16:01:50 +0700

    feature(Model): added enum Language

---
+++ Lab1/Model/Language.cs
@@ -0,0 +1,23 @@
+namespace Model
+{
+    /// <summary>
+    /// Variants of languages.
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+        /// English language.
+        /// </summary>
+        English,
+
+        /// <summary>
+        /// Russian language.
+        /// </summary>
+        Russian,
+
+        /// <summary>
+        /// The language is not recognized.
+        /// </summary>
+        Unknown
+    }
+}
