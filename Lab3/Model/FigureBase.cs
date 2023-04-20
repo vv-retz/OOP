e473d3170ab984ad93b4acc27ea19f571945218a
Vladislav Rets <retz.vv@gmail.com>
2023-04-20 13:39:10 +0700

    feature(Lab3): intermediate commit

---
+++ Lab3/Model/FigureBase.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// FigureBase class.
+    /// </summary>
+    public abstract class FigureBase
+    {
+        /// <summary>
+        /// Count area of figure.
+        /// </summary>
+        protected abstract double FigureArea { get; }
+
+        /// <summary>
+        /// Method which check sign of lenght of line segment.
+        /// </summary>
+        /// <param name="lineSegment">Length of the segment.</param>
+        /// <returns>Length of the line segment.</returns>
+        protected double CheckValue(double lineSegment)
+        {
+            return lineSegment <= 0
+                ? throw new ArgumentException("The length of the" +
+                " segment must be positive.")
+                : lineSegment;
+        }
+    }
+}
---
+++ Lab3/Model/ICalculatable.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Interface ICalculatable.
+    /// </summary>
+    public interface ICalculatable
+    {
+        /// <summary>
+        /// Method which calculate something.
+        /// </summary>
+        void Calculate();
+    }
+}
